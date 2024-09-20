9603bdf04ec9e2da81d19056661367079a5c74f0
Matheus-Bertolini <nunesmath890@gmail.com>
2024-09-20 10:51:31 -0300

    criado o LoginVM, RegistroVM e UsuarioVM

---
+++ GCook/ViewModels/LoginVM.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GCook.ViewModels;
+    public class LoginVM
+    {
+        [Display(Name = "Email ou Nome de Usuário", Prompt = "Informe seu Email ou Nome de Usuário")]
+        [Required(ErrorMessage = "Por favor, informe seu email ou nome de usuário")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha de Acessi", Prompt = "******")]
+        [Required(ErrorMessage = "Por favor, informe sua senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Display(Name = "Manter Conectado?")]
+        public bool Lembrar { get; set; } = false;
+
+        public string UrlRetorno { get; set; }
+    }
---
+++ GCook/ViewModels/RegistroVM.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GCook.ViewModels;
+
+    public class RegistroVM
+    {
+        [Display(Name = "Nome Completo", Prompt = "Informe seu Nome Completo")]
+        [Required(ErrorMessage = "Por favor, informe seu Nome")]
+        [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 60 caracteres")]
+        public string Nome { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Nascimento", Prompt = "Informe sua Data de Nascimento")]
+        [Required(ErrorMessage = "Por favor, informe sua Data de Nascimento")]
+        public DateTime? DataNascimento { get; set; } = null;
+        
+        [Display(Prompt = "Informe seu Email")]
+        [Required(ErrorMessage = "Por favor, informe seu Email")]
+        [EmailAddress(ErrorMessage = "Por favor, informe um Email Válido!")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "A Senha deve possuir no mínimo 6 e no máximo 20 caracteres")]
+        public string Senha { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha de Acesso", Prompt = "Confirme sua Senha de Acesso")]
+        [Compare("Senha", ErrorMessage = "As Senhas não conferem.")]
+        public string ConfirmacaoSenha { get; set; }
+
+        public IFormFile Foto { get; set; }
+        public bool Termos { get; set; } = false;
+        public bool Enviado { get; set; } = false;        
+    }
